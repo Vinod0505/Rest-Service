db0cd0e4e5a60e317e6487e602197a0c0f847072
vinod0505 <vinodbojaraju@gmail.com>
2024-08-28 12:18:27 +0530

    fifth commit

--- src/main/java/com/hpe/restservice/controller/EmployeeController.java
+++ src/main/java/com/hpe/restservice/controller/EmployeeController.java
@@ -2,9 +2,12 @@ package com.hpe.restservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hpe.restservice.model.Employee;
 import com.hpe.restservice.utility.EmployeeManager;
 import com.hpe.restservice.utility.Employees;
 
@@ -12,12 +15,17 @@ import com.hpe.restservice.utility.Employees;
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeManager employeeManager;
 
 	@GetMapping("/employees")
 	public Employees getEmployees() {
 		return employeeManager.getEmployees();
+	}
+
+	@PostMapping("/employees")
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeManager.addEmployee(employee);
 	}
 }
